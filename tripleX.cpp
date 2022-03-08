bf1536dd621fd30fe0c067ef259806416324892c
BobTarino <bob.tarino@gmail.com>
2022-03-08 16:14:37 -0600

    learned how to print strings to terminal and how to feed new lines

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are a robotics engineer trapped in a conference room with an AI system gone rogue";
+    std::cout << std::endl; // ends text line
+    std::cout << "You need to disable every robot in the room or else the board will be terminated...";
     return 0;
 }
