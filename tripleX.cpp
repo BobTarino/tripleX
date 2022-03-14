343cde747b04ad70908c379b521726bae2d4f1bd
BobTarino <bob.tarino@gmail.com>
2022-03-14 16:20:54 -0500

    drfined int variables and printed to terminal

--- tripleX.cpp
+++ tripleX.cpp
@@ -4,6 +4,18 @@ int main()
 {
     std::cout << "You are a robotics engineer trapped in a conference room with an AI system gone rogue";
     std::cout << std::endl; // ends text line
-    std::cout << "You need to disable every robot in the room or else the board will be terminated...";
+    std::cout << "Enter the correct code to disable every robot in the room or else the board will be terminated...";
+    
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl; 
+    std::cout << sum << std::endl; 
+    std::cout << product << std::endl;
+
     return 0;
 }
