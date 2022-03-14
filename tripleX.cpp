16817ea7d6d24a7b461d5710edd89bcc3f61947e
BobTarino <bob.tarino@gmail.com>
2022-03-14 16:36:53 -0500

    const variables & assigning value

--- tripleX.cpp
+++ tripleX.cpp
@@ -6,12 +6,14 @@ int main()
     std::cout << std::endl; // ends text line
     std::cout << "Enter the correct code to disable every robot in the room or else the board will be terminated...";
     
-    int a = 4;
-    int b = 3;
-    int c = 2;
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
 
     std::cout << std::endl; 
     std::cout << sum << std::endl; 
